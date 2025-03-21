a8270a76b5eb1aaffbea338f682a5d94ca4f286b
nicolasshul <nshulga@gilbreth-fe01.rcac.purdue.edu>
2025-03-20 23:07:55 -0400

    removed build and fixed CMakes

--- EzPC/SCI/cmake/install_Eigen3.cmake
+++ EzPC/SCI/cmake/install_Eigen3.cmake
@@ -1,4 +1,4 @@
-find_package(Eigen3 3.3 NO_MODULE QUIET)
+find_package(Eigen3 NO_MODULE QUIET PATH ${PROJECT_SOURCE_DIR}/extern/Eigen3)
 if (NOT Eigen3_FOUND)
     message(STATUS "Eigen 3.3 was not found: clone and install Eigen3 locally")
     if (NOT EXISTS "${PROJECT_SOURCE_DIR}/extern/eigen/CMakeLists.txt")
@@ -8,11 +8,11 @@ if (NOT Eigen3_FOUND)
                 WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}")
     endif ()
     execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory build
-        WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/extern/eigen/")
+	    WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/extern/eigen/")
     execute_process(COMMAND ${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=${PROJECT_SOURCE_DIR}/build ..
         WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/extern/eigen/build")
     execute_process(COMMAND ${CMAKE_COMMAND} --build .. --target install
         WORKING_DIRECTORY "${PROJECT_SOURCE_DIR}/extern/eigen/build")
     message(STATUS "${PROJECT_SOURCE_DIR}")
-    find_package(Eigen3 3.3 REQUIRED NO_MODULE PATHS "${PROJECT_SOURCE_DIR}/build/")
+    find_package(Eigen3 NO_MODULE PATHS "${PROJECT_SOURCE_DIR}/extern/Eigen3")
 endif ()
